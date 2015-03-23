fff9fce5caa6e641b66d5ab1decf46f1f6df9e2e
wolfboyft <henryfthomson@gmail.com>
2015-03-23 14:10:16 +0000

    Updated version.
    Will compile and upload promptly.

--- main/java/wolfboyft/bettermushroom/Reference.java
+++ main/java/wolfboyft/bettermushroom/Reference.java
@@ -3,7 +3,7 @@ package wolfboyft.bettermushroom;
 public class Reference {
 	public static final String MOD_ID = "bettermushroom";
 	public static final String MOD_NAME = "Better Mushroom Islands";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	public static final String CLIENT_PROXY_CLASS = "wolfboyft.bettermushroom.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "wolfboyft.bettermushroom.proxy.CommonProxy";
 }
